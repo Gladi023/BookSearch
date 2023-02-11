57bd3c1a50f573246b9b4c5380c09b24a095f085
Gladi023 <Gladimar.Villamil@gmail.com>
2023-02-11 01:44:39 -0800

    auth.js updated

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,39 +1,41 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+const extractToken = (req) => {
+  const token = req.body.token || req.query.token || req.headers.authorization;
+  if (!token) {
+    return null;
+  }
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  return req.headers.authorization
+    ? token.split(' ').pop().trim()
+    : token;
 };
+
+const authMiddleware = (req) => {
+  const token = extractToken(req);
+  if (!token) {
+    return req;
+  }
+  
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+  } catch {
+    console.log('Invalid token');
+  }
+
+  return req;
+};
+
+const signToken = (payload) => {
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+};
+
+module.exports = {
+  authMiddleware,
+  signToken,
+};
+    
